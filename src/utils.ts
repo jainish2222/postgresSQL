24ab5f2f06e60fab0b003de86e84bdb4629bc78f
ParthKanani17 <jainishkoladiya.it21@scet.ac.in>
2024-03-09 13:26:48 +0530

    fr

--- src/utils.ts
+++ src/utils.ts
@@ -1,7 +1,7 @@
 import { Client } from 'pg';
 
 export async function getClient() {
-    const client = new Client("postgres://wzsxsnxg:LHZ9Cv4QoZ1zctxapkOq2ch672-o9UQe@trumpet.db.elephantsql.com/wzsxsnxg");
+    const client = new Client("postgres://xuspahio:QEBEZ_aWhZUKAH2_11zthtjMXTtwtpRG@flora.db.elephantsql.com/xuspahio");
     await client.connect();
     return client;
 }
